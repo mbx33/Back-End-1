5d9bf8c5f1ebf8a1bb9f8626dc3269f7402af7f0
Matt Baxter <mbx33@hotmail.com>
2021-08-02 13:00:27 -0600

    Initial

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+let friends = ['Rob', 'Max', 'Emily', 'Jennifer', 'Kayla', 'Daniel'];
+
+
+app.get('/api/users', (req, res) => {
+    res.status(200).send(friends)
+});
+
+// app.get("/weather/:temperature", (req, res) => {
+//     const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+//     res.status(200).send(phrase);
+//   });
+
+app.get("/weather/:temperature", (req, res) => {
+    const { temperature } = req.params;
+    const phrase = `<h3>It was ${temperature} today</h3>`
+    res.status(200).send(phrase)    
+})
+
+app.listen(4000, () => console.log("server running on 4000"));
+
+// app.listen(5500, () => console.log("server running on 5500"));
+
